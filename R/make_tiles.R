a673daf26c79b88e4ec05d1e690c1a7553a2b6cd
Eric Scott <scottericr@gmail.com>
2024-05-15 18:51:17 -0700

    create dir

--- R/make_tiles.R
+++ R/make_tiles.R
@@ -1,7 +1,8 @@
 make_tiles <- function(rast) {
   rast_name <- as.character(rlang::ensym(rast))
-  x <- terra::rast(ncols = 3, nrows = 2) #6 tiles 
+  x <- terra::rast(ncols = 3, nrows = 2) #6 tiles
   ext(x) <- ext(rast)
-  makeTiles(rast, x, filename = fs::path("data", "tiles", path_ext_set(rast_name, "tiff")), overwrite = TRUE)
+  fs::dir_create("tiles")
+  makeTiles(rast, x, filename = fs::path("tiles", path_ext_set(rast_name, "tiff")), overwrite = TRUE)
 }
 
