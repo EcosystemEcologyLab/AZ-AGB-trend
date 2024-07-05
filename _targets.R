0c0a82b4c149c2b32b598dbe7840cf70faf34cb9
Eric Scott <scottericr@gmail.com>
2024-07-05 15:25:47 -0700

    working on re-calculating slopes

--- _targets.R
+++ _targets.R
@@ -6,7 +6,7 @@
 # Load packages required to define the pipeline:
 library(targets)
 library(tarchetypes)
-library(geotargets)
+library(geotargets) #dev version for tar_terra_tiles()
 library(rlang) #for syms()
 library(crew)
 library(quarto) #only required for rendering reports
@@ -22,7 +22,7 @@ controller_local <-
 
 # Set target options:
 tar_option_set(
-  packages = c("ncdf4", "terra", "fs", "purrr", "car", "dplyr"), # Packages that your targets need for their tasks.
+  packages = c("ncdf4", "terra", "geotargets", "fs", "purrr", "car", "dplyr"), # Packages that your targets need for their tasks.
   controller = controller_local,
   # improve memory performance
   memory = "transient", 
@@ -35,11 +35,13 @@ tar_option_set(
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
 
-#use local data in this project
-# root <- "data"
+## use data on mounted "snow" drive 
+## (reading files dircetly from snow is quite slow for me working from home over
+## the VPN, and I prefer to just copy them to my hard drive)
+# root <- "/Volumes/moore/"
 
-#use data on mounted "snow" drive (super slow and not advisable)
-root <- "/Volumes/moore/"
+#use local data in this project
+root <- "data"
 
 inputs <- tar_plan(
   # create shapefiles for southwest
@@ -62,8 +64,11 @@ inputs <- tar_plan(
   tar_file_fast(file_menlove,  fs::path(root, "AGB_cleaned/menlove/menlove_2009-2019.tif")),
   tar_file_fast(file_gedi,     fs::path(root, "AGB_cleaned/gedi/gedi_2019-2023.tif")),
   tar_file_fast(file_chopping, fs::path(root, "AGB_cleaned/chopping/chopping_2000-2021.tif")),
+  
+  # These are tiles
   tar_file_fast(dir_esa,       fs::path(root, "AGB_cleaned/esa_cci/")),
   tar_file_fast(dir_ltgnn,     fs::path(root, "AGB_cleaned/lt_gnn/")),
+  
   
   # Read in rasters (all layers for now)
   tar_terra_rast(agb_xu, read_agb(file_xu, az)),
@@ -71,71 +76,87 @@ inputs <- tar_plan(
   tar_terra_rast(agb_menlove, read_agb(file_menlove, az)),
   tar_terra_rast(agb_gedi, read_agb(file_gedi, az)),
   tar_terra_rast(agb_chopping, read_agb(file_chopping, az)),
+  
   tar_terra_rast(agb_esa, read_agb(dir_esa, az)),
   tar_terra_rast(agb_ltgnn, read_agb(dir_ltgnn, az))
 )
 
 # Calculate trends in AGB
-slopes_small <- tar_plan(
-  tar_map(#for each data product
-    values = list(
-      product = syms(c("agb_xu", "agb_liu"))
-    ),
-    #calculate slopes
-    tar_terra_rast(
-      slope, 
-      calc_slopes(product)
-    ),
-    # Then plot the slopes and export a .png
-    tar_target(
-      slope_plot,
-      plot_slopes(slope, region = az),
-      #packages only needed for plotting step:
-      packages = c("ggplot2", "tidyterra", "colorspace", "stringr", "ggtext")
-    )
-  )
+slopes <- tar_plan(
+  tar_terra_rast(slope_xu, calc_slopes(agb_xu)),
+  tar_terra_rast(slope_liu, calc_slopes(agb_liu)),
+  
+  #for higher res products, split into a few tiles for dynamic branching
+  tar_terra_tiles(tiles_agb_chopping, raster = agb_chopping, ncol = 3, nrow = 4),
+  tar_terra_rast(
+    tiles_slope_chopping,
+    calc_slopes(tiles_agb_chopping),
+    iteration = "list",
+    pattern = map(tiles_agb_chopping)
+  ), 
+  tar_terra_rast(slope_chopping, merge(sprc(tiles_slope_chopping)), description = "recombine tiles")
 )
-
-# for high resolution data products, need to break into tiles and do
-# computations by tile to not run out of memory
-slopes_big <- tar_plan(
-  tar_map(# for each data product
-    values = list(
-      product = syms(c("agb_esa", "agb_chopping", "agb_ltgnn"))
-    ),
-    # split into tiles on disk
-    tar_target(
-      tiles,
-      make_tiles(product),
-    ),
-    # track the resulting files
-    tar_target(
-      tiles_files,
-      tiles,
-      pattern = map(tiles),
-      format = "file"
-    ),
-    # iterate over files to read them in and calculate slopes
-    tar_terra_rast(
-      slope_tiles,
-      calc_slopes(terra::rast(tiles_files)),
-      pattern = map(tiles_files),
-      iteration = "list"
-    ),
-    # merge tiles together
-    tar_terra_rast(
-      slope,
-      slope_tiles |> sprc() |> merge()
-    ),
-    # Then plot the slopes and export a .png
-    tar_target(
-      slope_plot,
-      plot_slopes(slope, region = az),
-      #packages only needed for plotting step:
-      packages = c("ggplot2", "tidyterra", "colorspace", "stringr", "ggtext")
-    )
-  )
-)
+# 
+# slopes_small <- tar_plan(
+#   tar_map(#for each data product
+#     values = list(
+#       product = syms(c("agb_xu", "agb_liu"))
+#     ),
+#     #calculate slopes
+#     tar_terra_rast(
+#       slope, 
+#       calc_slopes(product)
+#     ),
+#     # Then plot the slopes and export a .png
+#     tar_target(
+#       slope_plot,
+#       plot_slopes(slope, region = az),
+#       #packages only needed for plotting step:
+#       packages = c("ggplot2", "tidyterra", "colorspace", "stringr", "ggtext")
+#     )
+#   )
+# )
+# 
+# # for high resolution data products, need to break into tiles and do
+# # computations by tile to not run out of memory
+# slopes_big <- tar_plan(
+#   tar_map(# for each data product
+#     values = list(
+#       product = syms(c("agb_esa", "agb_chopping", "agb_ltgnn"))
+#     ),
+#     # split into tiles on disk
+#     tar_target(
+#       tiles,
+#       make_tiles(product),
+#     ),
+#     # track the resulting files
+#     tar_target(
+#       tiles_files,
+#       tiles,
+#       pattern = map(tiles),
+#       format = "file"
+#     ),
+#     # iterate over files to read them in and calculate slopes
+#     tar_terra_rast(
+#       slope_tiles,
+#       calc_slopes(terra::rast(tiles_files)),
+#       pattern = map(tiles_files),
+#       iteration = "list"
+#     ),
+#     # merge tiles together
+#     tar_terra_rast(
+#       slope,
+#       slope_tiles |> sprc() |> merge()
+#     ),
+#     # Then plot the slopes and export a .png
+#     tar_target(
+#       slope_plot,
+#       plot_slopes(slope, region = az),
+#       #packages only needed for plotting step:
+#       packages = c("ggplot2", "tidyterra", "colorspace", "stringr", "ggtext")
+#     )
+#   )
+# )
 
 # Calculate summary stats for each slope raster
 data <- tar_plan(
@@ -194,8 +215,9 @@ render <- tar_plan(
 
 #_targets.R must end with a list of targets.  They can be arbitrarily nested
 list(inputs,
-     slopes_small,
-     slopes_big,
+     slopes,
+     # slopes_small,
+     # slopes_big,
      data,
      stats,
      plot,
